5b386f41de0b81557f68e8b44cab04def5a857e2
Keith <keithbeste@northwwesternmutual.com>
2017-05-18 15:38:32 -0500

    adding stuff

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,7 +1,7 @@
 module.exports = (function(){
     return {
         allTasks: function(req, res){
-            res.json({completed:false, description:'work being done', dueDate: new Date(), owner: 'collin', id: '1', submitDate: new Date()})
+            res.json([{completed:false, description:'work being done', dueDate: new Date(), owner: 'collin', id: '1', submitDate: new Date()}])
         }
     }
 })();
