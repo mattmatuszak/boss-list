bfa4a123e58cd5fc98cc5c53ed4c8ebea9037523
KOSINSKI, COLLIN <collinkosinski@northwesternmutual.com>
2017-05-18 12:58:17 -0500

    added tasks api

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,5 @@
+var api = require('../controllers/api.js')
+
 module.exports = function(app){
-    app.get('/api/v1', function(req , res){
-        res.json({message:'yoo'})
-    })
+    app.get('/api/v1/tasks', api.allTasks);
 };
