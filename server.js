d9fce9f8a2877dbcc977b3db6825def99e1e90b4
Matt Matuszak <mattmatuszak@northwesternmutual.com>
2017-05-18 22:27:36 -0500

    added semi-working sockets

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 app.use(bp.urlencoded({ extended: true }));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/socket.io-client/dist')))
 
-require('./server/config/routes.js')(app);
+
 
 var server = app.listen(port, function() {
   console.log("We running dis server yall")
@@ -16,18 +16,18 @@ var server = app.listen(port, function() {
 
 var io = require('socket.io').listen(server);
 
-io.sockets.on('connection', function(socket) {
-  console.log("WE ARE USING SOCKETS!");
-  console.log(`socket id:${socket.id}`);
-  //all the socket code goes in here!
+// io.sockets.on('connection', function(socket) {
+//   console.log("WE ARE USING SOCKETS!");
+//   console.log(`socket id:${socket.id}`);
+//   //all the socket code goes in here!
+//
+//   socket.on("allTableDataLoaded", function(data) {
+//     console.log('Loaded all the data and setup their socker!  Reason: ' + data.reason);
+//     socket.emit('allTableDataLoadedResponse', {
+//       response: "great, I'm glad this worked out for you!"
+//     });
+//   });
+// });
 
-  socket.on("allTableDataLoaded", function(data) {
-    console.log('Loaded all the data and setup their socker!  Reason: ' + data.reason);
-    socket.emit('allTableDataLoadedResponse', {
-      response: "great, I'm glad this worked out for you!"
-    });
-  });
 
-
-
-});
+require('./server/config/routes.js')(app, io);
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,34 @@
 var api = require('../controllers/api.js')
+var mysocket = {message: 'dummyobject'};
 
-module.exports = function(app){
-    app.get('/api/v1/tasks', api.allTasks);
+module.exports = function(app, io) {
 
-    app.post('/api/v1/tasks', api.addTask);
+  io.sockets.on('connection', function(socket) {
+    console.log("WE ARE USING SOCKETS!");
+    console.log(`socket id:${socket.id}`);
+    //all the socket code goes in here!
+    mysocket = socket;
+
+    socket.on("allTableDataLoaded", function(data) {
+      console.log('Loaded all the data and setup their socker!  Reason: ' + data.reason);
+      socket.emit('allTableDataLoadedResponse', {
+        response: "great, I'm glad this worked out for you!"
+      });
+    });
+  });
+
+
+
+  app.get('/api/v1/tasks', function(request, response) {
+    api.allTasks(request, response);
+  });
+
+  app.post('/api/v1/tasks', function(request, response, next) {
+    api.addTask(request, response);
+    next();
+  }, function(request, response, next) {
+    console.log('after tasks post with data' + JSON.stringify(response.req.body));
+    mysocket.broadcast.emit('newTask', response.req.body);
+    next();
+  });
 };
